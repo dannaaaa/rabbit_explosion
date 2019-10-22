43d6a0cb451f96014fd43d6216fc1f15edc9224e
Dana Korang-Awua <danaawua@hotmail.co.uk>
2019-10-22 15:27:21 +0100

    Initial commit

---
+++ src/test/java/com/spartaglobal/rabbit_explosion/AppTest.java
@@ -0,0 +1,20 @@
+package com.spartaglobal.rabbit_explosion;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
